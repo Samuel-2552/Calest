800acf87a675d63ed1fa5c17fc8d2c12813684a9
Samuel <201501502@rajalakshmi.edu.in>
2022-11-04 16:48:20 +0530

    Added Icon

--- app/src/main/java/com/example/calest/MainActivity.java
+++ app/src/main/java/com/example/calest/MainActivity.java
@@ -14,6 +14,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -91,6 +92,8 @@ public class MainActivity extends AppCompatActivity {
 
                 // Runs model inference and gets result.
                 MobilenetV110224Quant.Outputs outputs = model.process(inputFeature0);
+                Log.i("MainActivity","hello");
+                System.out.println(outputs);
 
                 TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
 
